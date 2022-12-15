f78e7a7749cb13ed5310849f878dbbde46ac6fe2
Mark Westerweel <mark.westerweel@hva.nl>
2022-12-15 21:27:12 +0100

    I'm refactoorring

--- fahrenheit/src/main.rs
+++ fahrenheit/src/main.rs
@@ -1,59 +1,58 @@
 use std::io;
 
 fn main() {
+    println!("Welcome to the degrees converter!");
     
-    intro();
+    let metric = get_metric();
+    let degrees =  get_degrees();
 
+    check_metric(metric, degrees);
+}
+
+
+fn get_metric() -> u32{
+    
+    println!("Please enter a metric");
+    println!("1. Celsius");
+    println!("2. Fahrenheit");
+    
     let mut metric = String::new();
-
-    
+        
     io::stdin()
     .read_line(&mut metric)
     .expect("Failed to get a metric");
     
     let metric: u32 = metric.trim().parse().expect("That's not a number");
+    return metric;
+}
 
+fn get_degrees() -> f32 {
 
     println!("Please enter your degrees");
-    
+
     let mut degrees = String::new();
     
     io::stdin()
     .read_line(&mut degrees)
     .expect("Failed to read line");
 
-    let degrees: u32 = degrees.trim().parse().expect("That's not a number");
-
-    println!("Your input is :  {degrees} degrees {metric}");
-
-    check_metric(metric, degrees);
-
-
+    let degrees: f32 = degrees.trim().parse().expect("That's not a number");
+    return degrees;
 }
 
-
-fn intro(){
-    println!("Welcome to the degrees converter!");
-    println!("Please enter a metric");
-    println!("1. Celsius");
-    println!("2. Fahrenheit");
-}
-
-
-fn celsius_to_fahrenheit(degrees: u32) -> f32{
+fn celsius_to_fahrenheit(degrees: f32) -> f32{
     degrees as f32 * 1.8 + 32.0
 }
 
 
-fn fahrenheit_to_celsius(degrees :u32) -> f32{
+fn fahrenheit_to_celsius(degrees:f32) -> f32{
     degrees as f32 / 1.8 - 32.0
 }
 
-fn check_metric(metric: u32, degrees: u32){
+fn check_metric(metric: u32, degrees: f32){
     if metric == 1 { 
-        println!("That's { } in fahrenheit", celsius_to_fahrenheit(degrees))
+        println!("That's {:.1} degrees in fahrenheit", celsius_to_fahrenheit(degrees))
     } else if metric == 2 {
-        println!("That's { } in celsius", fahrenheit_to_celsius(degrees))
+        println!("That's {:.1} degrees in celsius", fahrenheit_to_celsius(degrees))
     }
 }
-
