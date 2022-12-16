7c0371587c0d6d1016e5802b1e1ed3d4c6a90fa1
Mark Westerweel <mark.westerweel@hva.nl>
2022-12-16 18:36:11 +0100

    little clean up

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -12,29 +12,22 @@ fn main() {
     let fib : usize = fib.trim().parse().expect("That's not a number");
 
     // place to store the fib sequence
-    let mut fib_sequence = Vec::new();
+    let mut fib_sequence = vec![0u128, 1u128, 2u128];
 
-    let mut counter = 3u128;
-    let start = 0u128;
-    let mut prev  = 1u128;
-    let mut last = 2u128;
-    let mut next : u128 = 0u128;
-    let mut current_fib = 0u128;
-
-    fib_sequence.push(start);
-    fib_sequence.push(prev);
-    fib_sequence.push(last);
+    let mut prev  = fib_sequence[1];
+    let mut last = fib_sequence[2];
+    let mut next : u128;
     
+    // will loop as long as fib request 
+    let mut counter = 3u128;
     while counter != fib as u128{
 
-        println!("{} {} ", prev, last);
         next = prev + last;
         fib_sequence.push(next);
-        println!("{:?}", fib_sequence);
+        // set the variables for next iteration
         prev = last;
-
         last = next;
-        
+
         counter +=1;
     }
 
